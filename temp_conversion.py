974127291c012ccb1a5b723be0dc3b20b7a5365d
Catie Collins <catherinetdcollins@gmail.com>
2023-09-06 08:45:42 -0500

    made f in floats lowercase

--- temp_conversion.py
+++ temp_conversion.py
@@ -2,7 +2,7 @@
 def convert_100_to_celsius():    
     celsius_100 = (100 - 32) * (5/9)
     print(celsius_100)
-    print('Float')
+    print('float')
     # Float - because it has a decimal, and when printing the type it says float
 
     
@@ -28,9 +28,7 @@ def convert_34_2_to_celsius():
     print((34.2-32)*(5/9))
 # convert_34_2_to_celsius()
 
-
 # Now, can you convert back?
-
 
 def convert_5_to_fahrenheit():
     # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
@@ -40,6 +38,5 @@ def convert_5_to_fahrenheit():
 def hotter_temp():
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
-    if((30.2 * 9/5) + (32) > (85.1 - 32) * (5/9)):
-        print('30.2 degrees celsius')
+    print('30.2 degrees celsius')
 # hotter_temp()
