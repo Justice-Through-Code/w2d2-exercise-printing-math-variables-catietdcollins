18f4d965f604f80586e497d157725c03e1f81666
Catie Collins <catherinetdcollins@gmail.com>
2023-09-06 08:39:39 -0500

    commented out converts

--- temp_conversion.py
+++ temp_conversion.py
@@ -12,7 +12,7 @@ def convert_100_to_celsius():
     # Print 'float' if it is a float or 'int' if it is an int
     # How do you know? Write a comment below your code explaining how you know which it is
 
-convert_100_to_celsius()
+# convert_100_to_celsius()
 
 def convert_0_to_celsius():
     celsius_0 = (0 - 32) * (5/9)
@@ -20,13 +20,13 @@ def convert_0_to_celsius():
     # Convert a temperature of 0 degrees fahrenheit to celsius
     # Save this to a variable called celsius_0, and use print() to print out the value
 
-convert_0_to_celsius()
+# convert_0_to_celsius()
 
 def convert_34_2_to_celsius():
     # Convert a temperature of 34.2 degrees fahrenheit to celsius
     # Do this one all in one print statement without saving any variables
     print((34.2-32)*(5/9))
-convert_34_2_to_celsius()
+# convert_34_2_to_celsius()
 
 
 # Now, can you convert back?
@@ -35,11 +35,11 @@ convert_34_2_to_celsius()
 def convert_5_to_fahrenheit():
     # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
     print((5 * 9/5) + (32))
-convert_5_to_fahrenheit()
+# convert_5_to_fahrenheit()
 
 def hotter_temp():
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
     if((30.2 * 9/5) + (32) > (85.1 - 32) * (5/9)):
         print('30.2 degrees celsius')
-hotter_temp()
+# hotter_temp()
